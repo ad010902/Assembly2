1674361e2cc56a04ef6a47ec207bef49ec20b8d1
admin <admin@DESKTOP-SLDS87R>
2024-04-18 23:20:02 +0700

    ..

--- change_file.py
+++ change_file.py
@@ -1,6 +1,6 @@
 # Define the input and output file paths
-input_file_path = "test.txt"
-output_file_path = "output_file.txt"
+input_file_path = "/home/duonnguyen/scaffolds.filled_clean.fa"
+output_file_path = "/home/duongnguyen/scaffolds.filled_clean_format.fa"
 
 try:
     # Open the input file for reading
